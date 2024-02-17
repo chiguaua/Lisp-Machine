14a573756b73c88c47d62249de65d43a3a065ae1
benmasud <rofikulbenmasud@gmail.com>
2024-02-17 22:04:15 +0700

    Test run -

---
+++ Lisp-Machine/src/test.lisp
@@ -0,0 +1,18 @@
+; test.lisp
+
+(defun add (a b)
+  (+ a b))
+
+(defun subtract (a b)
+  (- a b))
+
+(defun multiply (a b)
+  (* a b))
+
+(defun divide (a b)
+  (/ a b))
+
+(add 5 3)
+(subtract 10 4)
+(multiply 2 6)
+(divide 8 2)

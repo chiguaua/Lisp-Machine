aeda1f3929bf06c436f9ff8613256b2d19761c5d
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-04-01 14:11:40 +0700

    int -- object fix

--- src/main/java/TestOut.java
+++ src/main/java/TestOut.java
@@ -1,36 +1,45 @@
 import java.util.function.Function;
 
-public class TestOut {public static Object add(Object a, Object b) {
-    return (((Number) a).doubleValue()+((Number) b).doubleValue());
-}
+public class TestOut {
+    public static Object add(Object a, Object b) {
+        return (((Number) a).doubleValue() + ((Number) b).doubleValue());
+    }
+
     public static Object aAdd(Object a, Object b) {
-        return (((Number) (((Number) a).doubleValue()+((Number) b).doubleValue())).doubleValue()+((Number) a).doubleValue());
+        return (((Number) (((Number) a).doubleValue() + ((Number) b).doubleValue())).doubleValue() + ((Number) a).doubleValue());
     }
+
     public static Object subtract(Object a, Object b) {
-        return (((Number) a).doubleValue()-((Number) b).doubleValue());
+        return (((Number) a).doubleValue() - ((Number) b).doubleValue());
     }
+
     public static Object multiply(Object a, Object b) {
-        return (((Number) a).doubleValue()*((Number) b).doubleValue());
+        return (((Number) a).doubleValue() * ((Number) b).doubleValue());
     }
+
     public static Object divide(Object a, Object b) {
-        return (((Number) a).doubleValue()/((Number) b).doubleValue());
+        return (((Number) a).doubleValue() / ((Number) b).doubleValue());
     }
+
     public static Object factorial(Object a) {
-        if ((((Number) a).doubleValue()>((Number) 1).doubleValue())) {
-            return (((Number) a).doubleValue()*((Number) factorial((((Number) a).doubleValue()-((Number) 1).doubleValue()))).doubleValue());
+        if ((((Number) a).doubleValue() > ((Number) 1).doubleValue())) {
+            return (((Number) a).doubleValue() * ((Number) factorial((((Number) a).doubleValue() - ((Number) 1).doubleValue()))).doubleValue());
 
         } else {
             return 1;
-        }}
+        }
+    }
+
     public static void main(String[] args) {
-        Function<Object, Object> lambdaFunction0 = (x) -> {return (((Number) x).doubleValue()*((Number) x).doubleValue());
-    };
+        Function<Object, Object> lambdaFunction0 = (x) -> {
+            return (((Number) x).doubleValue() * ((Number) x).doubleValue());
+        };
         lambdaFunction0.apply(5);
         add(3, 1);
         subtract(10, 4);
         multiply(2, 6);
         divide(8, 2);
-        System.out.println("dddqddq" );
+        System.out.println("dddqddq");
         Object x = 10;
         Object y = 5;
         System.out.println(add(x, y));
